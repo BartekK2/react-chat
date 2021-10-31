26aa73dffb9f2fabf2b219fda1cd50c6c07753ea
Bartuś Bartuś <barteksp82@gmail.com>
2021-11-01 00:08:40 +0100

    small changes

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -49,7 +49,7 @@ function RegisterForm() {
 
     return (
         <>
-            {currentUser&&<Redirect to="/react-chat" />}
+            {currentUser&&<Redirect to="/" />}
             <Grid
                 container
                 direction="column"
